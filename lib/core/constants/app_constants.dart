13e727b5af256799a175174f1e8bf0426b75535e
Soonhyeong Kwon <koreanhole1@gmail.com>
2026-03-13 22:55:51 +0900

    chore: Update `baseUrl` to `https://api.carai.ai.kr/`.

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,6 +1,5 @@
 class AppConstants {
   static const double mobileMaxWidth = 500.0;
-  static const String inviteBaseUrl = 'https://m.carai.ai.kr/invites';
-  static const String baseUrl = 'https://34.64.161.110:8080/';
+  static const String apiBaseUrl = 'https://api.carai.ai.kr/';
   static const String inviteBaseUrl = 'https://m.carai.ai.kr/invites';
 }
